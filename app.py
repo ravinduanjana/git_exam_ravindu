c5e8975ad2cdab5551b7ff002ee772095c1044ac
ravinduanjana <anjanaravindu698@gmail.com>
2025-06-15 12:57:02 +0530

    Fixes #1 - corrected the print message in dummy_function

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 def dummy_function():
     print('This is a dummy function')
     print('This is the version from the master branch.')
+
+    def dummy_function():
+    print("This message has been fixed properly.")
